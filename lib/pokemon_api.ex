53aa046856344157aa600fb051780b23b5855b81
brandaoplaster <brandaoplaster@gmail.com>
2021-01-20 23:21:20 -0300

    feat: adding module trainer

--- lib/pokemon_api.ex
+++ lib/pokemon_api.ex
@@ -1,9 +1,5 @@
 defmodule PokemonApi do
-  @moduledoc """
-  PokemonApi keeps the contexts that define your domain
-  and business logic.
+  alias PokemonApi.Trainer
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_trainer(params), to: Trainer.Create, as: :call
 end
