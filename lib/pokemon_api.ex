64227625388b135bad9d30875167a8a0ebdef112
brandaoplaster <brandaoplaster@gmail.com>
2021-01-25 12:29:12 -0300

    chore: adding parse pokemon

--- lib/pokemon_api.ex
+++ lib/pokemon_api.ex
@@ -1,8 +1,10 @@
 defmodule PokemonApi do
-  alias PokemonApi.Trainer
+  alias PokemonApi.{PokeApi, Trainer}
 
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
   defdelegate delete_trainer(params), to: Trainer.Delete, as: :call
   defdelegate fetch_trainer(params), to: Trainer.Get, as: :call
   defdelegate update_trainer(params), to: Trainer.Update, as: :call
+
+  defdelegate fetch_pokemon(params), to: Pokemon.Update, as: :call
 end
