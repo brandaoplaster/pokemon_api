7d182d213ae3bff32e01aea9747334679effe025
brandaoplaster <brandaoplaster@gmail.com>
2021-02-01 22:30:43 -0300

    fix: Correcting error in the create_trainer function

--- lib/pokemon_api/trainer/create.ex
+++ lib/pokemon_api/trainer/create.ex
@@ -9,5 +9,5 @@ defmodule PokemonApi.Trainer.Create do
 
   defp create_trainer({:ok, struct}), do: Repo.insert(struct)
 
-  defp create_trainer({:ok, _changeset} = error), do: error
+  defp create_trainer({:error, _changeset} = error), do: error
 end
