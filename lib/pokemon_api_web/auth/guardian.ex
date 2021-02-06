38708f382c8b1c6e27b6f26a7404c26a11a1bd88
brandaoplaster <brandaoplaster@gmail.com>
2021-02-06 15:31:19 -0300

    feat: adding trainer authentication with password

--- lib/pokemon_api_web/auth/guardian.ex
+++ lib/pokemon_api_web/auth/guardian.ex
@@ -1,5 +1,7 @@
 defmodule PokemonWeb.Auth.Guardian do
   use Guardian, otp_app: :pokemon_api
+
+  alias PokemonApi.{Repo, Trainer}
   
   def subject_for_token(trainer, _claims) do
     sub = to_string(trainer.id)
@@ -10,5 +12,24 @@ defmodule PokemonWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> PokemonApi.fetch_trainer()
+  end
+
+  def authentication(%{"id" => trainer_id, "password" => password}) do
+    case Repo.get(Trainer, trainer_id) do
+      nil -> {:error, "Trainer not found"}
+      trainer -> validate_password(trainer, password)
+    end
+  end
+
+  def validate_password(%Trainer{password_hash: hash} = trainer, password) do
+    case Argon2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, :unathorized}
+    end
+  end
+
+  defp create_token(trainer) do
+    {:ok, token, _claims} = encode_and_sign(trainer)
+    {:ok, token}
   end
 end
